12177c9c9b6d25e57f77a83e1acc755b8cd8a008
Ben Lower <github@benlower.us>
2023-11-21 13:55:21 -0800

    Swap to foxie query

--- fixieAssistant.js
+++ fixieAssistant.js
@@ -3,7 +3,6 @@
 //-------------------------------------------//
 import OpenAI from 'openai';
 import 'dotenv/config';
-import fetch from 'node-fetch';
 import { FixieClient } from "fixie";
 
 
@@ -14,9 +13,11 @@ const OPENAI_MODEL = "gpt-4-1106-preview";
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 const fixieClient = new FixieClient({ apiKey: process.env.FIXIE_API_KEY });
 
+const FIXIE_MAX_CHUNKS = 2;
 const POLL_INTERVAL = 100;
 const ASSISTANT_NAME = "Fixie Assistant";
-const SYSTEM_MESSAGE = "You are a helpful assistant who is an expert on a company called Fixie.ai. Fixie provides a platform for helping developers build conversational, AI applications. You have access to a knowledge base that you can query for more information about Fixie, their products, and their APIs.";
+// const SYSTEM_MESSAGE = "You are a helpful assistant who is an expert on a real company called Fixie.ai. The company is based in Seattle, WA and has a website at https://fixie.ai. Fixie provides a platform for helping developers build conversational, AI applications. You have access to a knowledge base that you can query for more information about Fixie, their products, and their APIs."; // Fixie
+const SYSTEM_MESSAGE = "You are a helpful assistant who is an expert on all types of foxes. You have access to a knowledge base that you can query for more information when users ask questions about foxes."; // Foxes
 const QUERY_FIXIE_CORPUS = {
   "name": "query_Fixie_Corpus",
     "parameters": {
@@ -34,49 +35,37 @@ const QUERY_FIXIE_CORPUS = {
     "description": "Query a knowledge base of information about Matt Welsh."
 };
 
-const TOOLS = [
-  { "type": "function", "function": QUERY_FIXIE_CORPUS }
-];
+const TOOLS = [{ "type": "function", "function": QUERY_FIXIE_CORPUS }];
 
-const USER_MESSAGES = [
-  { role: "user", content: "What does Fixie provide?" },
-  { role: "user", content: "How does the Corpus API work?"}
-];
-const FIXIE_API_URL = "https://api.fixie.ai";
-const FIXIE_CORPUS_ID = "437594d6-ae69-4e54-abea-c58ab2be80ec";
+// const USER_MESSAGE = { role: "user", content: "What is blue?"}; // works
+// const USER_MESSAGE = { role: "user", content: "How does the Corpus API work?"};  // fails
+// const USER_MESSAGE = { role: "user", content: "What does Fixie do?"}; // fails
+const USER_MESSAGE = { role: "user", content: "Who is Foxie?"};
 
+// const FIXIE_CORPUS_ID = "437594d6-ae69-4e54-abea-c58ab2be80ec";   // Fixie.ai
+const FIXIE_CORPUS_ID = "44094d5a-f817-4c2e-a2a4-8f8a0c936d0f";   // Foxes
 
 
 //-------------------------------------------//
-// 2.a Function to call Fixie Corpus
+// 2.a Call the Fixie Corpus API
 //-------------------------------------------//
 async function query_Fixie_Corpus(query) {
-  const queryResult = await fixieClient.queryCorpus({ corpusId: FIXIE_CORPUS_ID, query: query, maxChunks: 5 });
-
+  const queryResult = await fixieClient.queryCorpus({ corpusId: FIXIE_CORPUS_ID, query: query, maxChunks: FIXIE_MAX_CHUNKS });
   return queryResult;
-  // return result;
-  // const url = new URL(`api/v1/corpora/${FIXIE_CORPUS_ID}/query`, FIXIE_API_URL)
-  // const requestBody = {
-  //   corpusId: FIXIE_CORPUS_ID,
-  //   query: query,
-  // };
-  // const response = await fetch(url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Accept': 'application/json',
-  //     Authorization: `Bearer ${process.env.FIXIE_API_KEY}`,
-  //   },
-  //   body: JSON.stringify({ requestBody }),
-  // });
-  // if (!response.ok) {
-  //   throw new Error(`Corpus query request failed: ${response.status} ${response.statusText} ${JSON.stringify(response.body)}`);
-  // }
-  // return response.text();
 }
 
+// async function processFixieChunks(results) {
+//   let completeResults = "";
+//   const content = results.results;
+//   for (let i = 0; i < content.length; i++) {
+//     completeResults += content[i].chunkContent;
+//   } 
+
+//   return completeResults;
+// }
+
 async function processFixieChunks(results) {
-  const completeResults = "";
+  let completeResults = "";
   for (const result of results) {
     completeResults += result.chunkContent;
   }
@@ -103,12 +92,12 @@ const thread = await openai.beta.threads.create()
 //-------------------------------------------//
 // 5. Add Messages to the Thread
 //-------------------------------------------//
-// const message = await openai.beta.threads.messages.create(thread.id, USER_MESSAGES)
+const message = await openai.beta.threads.messages.create(thread.id, USER_MESSAGE);
 
-const message = await openai.beta.threads.messages.create(thread.id, {
-  role: 'user',
-  content: 'What is the Fixie corpus API?',
-})
+// const message = await openai.beta.threads.messages.create(thread.id, {
+//   role: 'user',
+//   content: 'What is the Fixie corpus API?',
+// })
 
 //-------------------------------------------//
 // 6. Run Assistant Loop (Polling)
@@ -149,7 +138,7 @@ async function runAssistant(interval) {
         if (functionName == "query_Fixie_Corpus") {
           const query = JSON.parse(functionArgs)["query"];
           const output = await query_Fixie_Corpus(query);
-          const processedOutput = await processFixieChunks(output);
+          const processedOutput = await processFixieChunks(output.results);
           tool_outputs.push({
             "tool_call_id": action["id"],
             "output": JSON.stringify(processedOutput)
@@ -180,6 +169,7 @@ async function runAssistant(interval) {
       // });
 
       console.log("Submitting function output back to the Assistant...");
+      console.log(`Tool Outputs: ${JSON.stringify(tool_outputs)}`);
       openai.beta.threads.runs.submitToolOutputs(thread.id, run.id, tool_outputs);
       break;
   
